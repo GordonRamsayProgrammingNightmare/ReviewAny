39ea328e369faa7aa185ddb1d3ac462e3a33cc20
ksy703 <ksy95703@naver.com>
2017-11-15 15:17:40 +0900

    Update upload.component.ts

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -7,6 +7,35 @@ import { Router } from '@angular/router';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
+  
+  customStyle = {
+    selectButton: {
+      "color": "black",
+      "height":"2.3em",
+      "border-radius":"0.2em",
+      "background-color":"#3cc",
+    },
+    clearButton: {
+      "color": "white",
+      "background-color": "#CC0033",
+      "height":"2.3em",
+      "border-radius":"0.2em",
+    },
+    layout: {
+      "background-image": "url('https://cdn0.iconfinder.com/data/icons/feather/96/camera-128.png')",
+      "background-repeat":"no-repeat",
+      "background-position":"center",
+      "background-size":"20% 20%",
+      "height":"20em",
+      "background-color": "white",
+      "color": "#gray",
+      "font-size": "15px",
+    },
+    previewPanel: {
+      
+      "background-color": "#white",
+    }
+  };
 
   constructor(
     private router: Router
